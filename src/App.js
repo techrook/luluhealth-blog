730e827b949ccdc61c529022c4ebde8e71f32d42
techrook <mondayitohowo3@gmail.com>
2022-04-18 21:53:19 +0100

    Init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../assets/scss/style.scss";
+import Header from "./components/Header";
+
+
+ function App() {
+
+  return (
+   <Header />
+      
+  );
+}
+export default App;
---
+++ src/components/Home.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <>
+    {/*header with slid in nav blogger photo*/}
+
+
+    {/*post (photo, dates ,author and lil image)*/}
+
+
+    {/*footer*/}
+
+
+
+
+    
+    
+    </>
+  )
+}
