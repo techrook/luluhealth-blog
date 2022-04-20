8bac4b56d2469c8ef34758891108e8fed6c4f4b5
techrook <mondayitohowo3@gmail.com>
2022-04-20 16:47:46 +0100

    init

---
+++ vite-react/src/App.jsx
@@ -0,0 +1,22 @@
+import { React } from 'react'
+import './App.css'
+import Header from '../src/components/Header'
+
+
+function App() {
+
+	return (
+		<>
+
+	    <p className="text-sm ...">The quick brown fox ...</p>
+<p className="text-base ...">The quick brown fox ...</p>
+<p className="text-lg ...">The quick brown fox ...</p>
+<p className="text-xl ...">The quick brown fox ...</p>
+<p className="text-6xl ...">The quick brown fox ...</p>
+    </>
+	
+	)
+}
+
+export default App
+	
---
+++ vite-react/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+export default function Header() {
+  return (
+    <>
+    <p className="text-sm ...">The quick brown fox ...</p>
+<p className="text-base ...">The quick brown fox ...</p>
+<p className="text-lg ...">The quick brown fox ...</p>
+<p className="text-xl ...">The quick brown fox ...</p>
+<p className="text-2xl ...">The quick brown fox ...</p>
+    </>
+
+  );
+}
